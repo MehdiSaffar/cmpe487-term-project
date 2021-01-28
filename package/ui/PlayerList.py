9c83ce06b9da68d69c77daffaf2f25e0e26633a2
Mehdi Saffar <saffar.mehdi3@gmail.com>
2021-01-29 00:23:28 +0300

    add PlayerList

---
+++ package/ui/PlayerList.py
@@ -0,0 +1,15 @@
+from ..constants import *
+
+
+class PlayerList:
+    def __init__(self):
+        pass
+
+    def handle_event(self, event):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
