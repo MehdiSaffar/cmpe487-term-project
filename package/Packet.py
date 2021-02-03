f3c6e1be1fe3a5fa423d821c2241dd45298e1daa
Mehdi Saffar <saffar.mehdi3@gmail.com>
2021-02-03 23:16:39 +0300

    handle game reply in SendRequestScene

--- package/Packet.py
+++ package/Packet.py
@@ -1,6 +1,21 @@
 def discover_packet(name, ip):
     return {
-        'TYPE': 'DISCOVER',
-        'NAME': name,
-        'IP': ip,
+        'type': 'discover',
+        'name': name,
+        'ip': ip,
+    }
+
+def game_request_packet(name, ip):
+    return {
+        'type': 'game_request',
+        'name': name,
+        'ip': ip,
+    }
+
+def game_reply_packet(name, ip, accept: bool):
+    return {
+        'type': 'game_reply',
+        'name': name,
+        'ip': ip,
+        'payload': accept,
     }
--- package/scenes/SendRequestScene.py
+++ package/scenes/SendRequestScene.py
@@ -1,10 +1,13 @@
+from package.Packet import game_request_packet
 from ..scenes.PlayScene import PlayScene
+from ..scenes.LobbyScene import LobbyScene
 import pygame
 from ..constants import *
 
 class SendRequestScene:
     def __init__(self, app):
         self.app = app
+
         pygame.display.set_caption('Waiting...')
         font = pygame.font.SysFont('Courier', 18, bold=True)
         
@@ -14,13 +17,33 @@ class SendRequestScene:
         
         # create a rectangular object for the
         # text surface object
-        self.textRect = self.text.get_rect()
+        self.text_rect = self.text.get_rect()
         
         # set the center of the rectangular object.
-        self.textRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
-        
+        self.text_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
+        self.send_game_request()
+
+    
+    def send_game_request(self):
+        addr = self.get_player_addr()
+        packet = game_request_packet(self.app.my_name, self.app.network.ip)
+        self.app.network.send(('tcp', addr, packet)) 
+    
+    def get_player_addr(self):
+        return (self.app.players[self.app.player_name]['ip'], self.app.network.udp_port)
+    
+    def handle_game_reply(self, event):
+        if event.data['payload']: # accepts
+            self.app.scene = PlayScene(self.app)
+        else:
+            self.app.scene = LobbyScene(self.app)
+
     def handle_event(self, event):
         if(event.type == 'udp'):
+            if (event.data['type'] == 'game_reply'):
+                self.handle_game_reply(event)
+
         if(event.type == pygame.MOUSEBUTTONDOWN):
             self.app.scene = PlayScene(self.app)
         
